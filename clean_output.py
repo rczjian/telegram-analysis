1a25444115112cd6b003c58097b0a904fc8939ac
Raymond Chan <rczjian@gmail.com>
2022-09-07 01:27:15 +0800

    init

---
+++ clean_output.py
@@ -0,0 +1,50 @@
+import io
+import sys
+
+try:
+    # Jupyter >= 4
+    from nbformat import read, write, NO_CONVERT
+except ImportError:
+    # IPython 3
+    try:
+        from IPython.nbformat import read, write, NO_CONVERT
+    except ImportError:
+        # IPython < 3
+        from IPython.nbformat import current
+    
+        def read(f, as_version):
+            return current.read(f, 'json')
+    
+        def write(nb, f):
+            return current.write(nb, f, 'json')
+
+
+def _cells(nb):
+    """Yield all cells in an nbformat-insensitive manner"""
+    if nb.nbformat < 4:
+        for ws in nb.worksheets:
+            for cell in ws.cells:
+                yield cell
+    else:
+        for cell in nb.cells:
+            yield cell
+
+
+def strip_output(nb):
+    """strip the outputs from a notebook object"""
+    nb.metadata.pop('signature', None)
+    for cell in _cells(nb):
+        if 'outputs' in cell:
+            cell['outputs'] = []
+        if 'prompt_number' in cell:
+            cell['prompt_number'] = None
+    return nb
+
+
+if __name__ == '__main__':
+    filename = sys.argv[1]
+    with io.open(filename, 'r', encoding='utf8') as f:
+        nb = read(f, as_version=NO_CONVERT)
+    nb = strip_output(nb)
+    with io.open(filename, 'w', encoding='utf8') as f:
+        write(nb, f)
